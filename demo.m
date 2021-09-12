692289114698abff90499ab4538fb99143f8d842
chyang913 <90540177+chyang913@users.noreply.github.com>
2021-09-12 18:38:08 +0800

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,15 @@
+clc; clear all; close all;
+
+img = im2double(imread('data/070.png'));
+
+param.gamma = 0.5 ;
+param.sky = true ;
+param.dcp = 0.95;
+
+dh_img = dehaze_imp(img,param);   
+
+outputDir='output/'; 
+if ~exist(outputDir, 'dir')
+    mkdir(outputDir)
+end   
+imwrite(dh_img,[outputDir 'Result_dehazed.png']); 
