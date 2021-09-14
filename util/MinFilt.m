4f2f927aef4bb2fc2bf5fbae86c7b0ab52e439b4
chyang913 <90540177+chyang913@users.noreply.github.com>
2021-09-14 08:44:20 +0800

    Delete MinFilt.m

--- util/MinFilt.m
+++
@@ -1,9 +0,0 @@
-function Y = MinFilt(X,N)
-% 2-D minimum filter using van-Herk algorithm
-% X is the input 2-D array
-% N is the 2-D patch size
-% Y is the minimum
-Y = VanHerkMin(X,N(1),'row');
-Y = VanHerkMin(Y,N(2),'column');
-    
-end
